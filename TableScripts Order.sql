f3ee133be8a519888ff0e6f824d31d42ddd007f9
viswanathamharshavardhan <harshavardhanteju@gmail.com>
2020-12-10 20:44:34 +0530

    Add files via upload

---
+++ TableScripts Order.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE `orderManagement_db`;
+
+use `orderManagement_db`;
+
+create table orderdetails(
+ORDERID int primary key,
+BUYERID int not null,
+AMOUNT decimal(10,2) not null,
+DATE date,
+ADDRESS varchar(100) not null,
+STATUS varchar(60) not null);
+
+create table productsordered(
+ORDERID int,
+PRODID int,
+SELLERID int not null,
+QUANTITY int not null,
+STATUS varchar(60) not null,
+price decimal(10,2),
+primary key(ORDERID,PRODID));
+
+show tables;
+
+drop table orderdetails;
+drop table productsordered;
+describe orderdetails;
+describe productsordered;
+
+
+INSERT INTO orderdetails (orderid,buyerid,amount,date,address,status)
+VALUES (1,11,12345,'2020-01-28','HYD','ORDER PLACED');
+INSERT INTO orderdetails (orderid,buyerid,amount,date,address,status)
+VALUES (2,12,999.00,'2020-02-28','Delhi','ORDER PLACED');
+
+INSERT INTO productsordered (orderid,prodid,sellerid,quantity,status,price)
+VALUES (1,1,4,1,'ORDER PLACED',12345);
+INSERT INTO productsordered (orderid,prodid,sellerid,quantity,status,price)
+VALUES (2,3,7,1,'ORDER PLACED',999.00);
+
+
+select * from orderdetails;
+
+select * from productsordered;
